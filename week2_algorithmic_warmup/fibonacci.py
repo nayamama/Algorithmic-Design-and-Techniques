607ad8c0ca68dc3186fe3de12a4e77816b8fb62b
Qi Yang <nayamama@hotmail.com>
2018-09-28 21:15:44 -0400

    add space saving method

--- week2_algorithmic_warmup/fibonacci.py
+++ week2_algorithmic_warmup/fibonacci.py
@@ -12,11 +12,21 @@ def calc_fib(n):
 # bottom_up method
 def fib_list(n):
     arr = [0, 1]
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         arr.append((arr[i - 1] + arr[i - 2]) % 10)
 
-    return arr[n] % 10
+    return arr[n]
+
+
+# space saving method
+def fib_rotation(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+
+    return a
 
 
 n = int(input())
-print(fib_list(n))
+# print(fib_list(n))
+print(fib_rotation(n))
